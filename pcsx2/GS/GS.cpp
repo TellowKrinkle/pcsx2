902c6e7cbef367587faedb74ba1a26537a2f5cde
Tellow Krinkle <tellowkrinkle@gmail.com>
2020-05-06 22:44:51 -0500

    GS: Add GL_ARB_copy_image fallback

--- pcsx2/GS/GS.cpp
+++ pcsx2/GS/GS.cpp
@@ -1469,6 +1469,7 @@ void GSApp::Init()
 	m_default_configuration["override_GL_ARB_texture_barrier"]            = "-1";
 	m_default_configuration["override_GL_ARB_shading_language_420pack"]   = "-1";
 	m_default_configuration["override_GL_ARB_buffer_storage"]             = "-1";
+	m_default_configuration["override_GL_ARB_copy_image"]                 = "-1";
 	m_default_configuration["OverrideTextureBarriers"]                    = "-1";
 	m_default_configuration["OverrideGeometryShaders"]                    = "-1";
 	m_default_configuration["paltex"]                                     = "0";
