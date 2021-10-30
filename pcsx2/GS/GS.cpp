6b070652c50c864cd9ba03d0ad09774b8e937c01
TellowKrinkle <tellowkrinkle@gmail.com>
2021-10-29 19:05:06 -0500

    Configuration option to disable messing with depth

--- pcsx2/GS/GS.cpp
+++ pcsx2/GS/GS.cpp
@@ -1215,6 +1215,7 @@ void GSApp::Init()
 	m_default_configuration["filter"]                                     = std::to_string(static_cast<s8>(BiFiltering::PS2));
 	m_default_configuration["force_texture_clear"]                        = "0";
 	m_default_configuration["fxaa"]                                       = "0";
+	m_default_configuration["fulldepth"]                                  = "0";
 	m_default_configuration["interlace"]                                  = "7";
 	m_default_configuration["conservative_framebuffer"]                   = "1";
 	m_default_configuration["linear_present"]                             = "1";
